0dc02057a33630902596f867eebe49c8112ddabe
Suikurx <mekhatra@icloud.com>
2023-03-23 18:49:55 -0400

    initial commit

---
+++ src/token_assets/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="blue window" id="logo">
+        <h1>
+          <span role="img" aria-label="tap emoji">
+          ☭
+          </span>
+          Marx
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
